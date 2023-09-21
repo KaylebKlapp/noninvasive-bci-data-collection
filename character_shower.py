da0a0e3abc8d7ce4605202016ed2e04d5596249f
Kayleb Klapp <kayleb114@gmail.com>
2023-09-21 09:35:19 -0600

    Some progress, i guess

---
+++ character_shower.py
@@ -0,0 +1,48 @@
+import pygame
+import numpy as np  
+from datetime import datetime
+import time
+import brainflow as bf
+import argparse
+
+
+pygame.init()
+pygame.font.init()
+font = pygame.font.SysFont("Alata", 700)
+
+time_keys = []
+letters = ['K', 'W', 'E']
+keys = [pygame.K_k, pygame.K_w, pygame.K_e]
+
+def render_ront(str, color = (0, 0, 0)):
+    text = font.render(str, True, (0,0,0))
+    return text
+
+def start_window():
+    pygame.display.set_caption("BCI training")
+    screen = pygame.display.set_mode((550, 500))
+    
+    letter_index = 0
+    running = True;
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False;
+                break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == keys[letter_index]:
+                    time_keys.append([letters[letter_index], datetime.now()])
+                    letter_index += 1
+                    letter_index %= len(letters)
+                    
+        screen.fill((205, 205, 205))
+        text = render_ront(letters[letter_index])
+        screen.blit(text, (50, 50))
+        pygame.display.flip();                    
+
+
+start_window()
+
+with open("test.csv", "w") as fp:
+    for input in time_keys:
+        fp.write(f"{input[0]},{input[1]}\n")
--- data_collection_p300.py
+++ data_collection_p300.py
@@ -1,43 +1,56 @@
-import pygame
+import numpy as np  
 from datetime import datetime
+import time
+import brainflow as bf
+import argparse
 
-pygame.init()
-pygame.font.init()
-font = pygame.font.SysFont("Alata", 700)
+# Fancy unnecessary network stuff
+# parser = argparse.ArgumentParser()
+# args = parser.parse_args()
 
-time_keys = []
-letters = ['K', 'W', 'E']
-keys = [pygame.K_k, pygame.K_w, pygame.K_e]
+# Define board and serial port
+board_type_id = bf.BoardIds.CYTON_BOARD
+port = '/dev/ttyUSB0'
+params = bf.BrainFlowInputParams()
+params.serial_port = port
+board = bf.BoardShim(board_type_id, params)
+board_id = board.get_board_id()
 
-def render_ront(str, color = (0, 0, 0)):
-    text = font.render(str, True, (0,0,0))
-    return text
+board.prepare_session()
 
-def start_window():
-    pygame.display.set_caption("BCI training")
-    screen = pygame.display.set_mode((550, 500))
-    
-    letter_index = 0
-    running = True;
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False;
-                break
-            elif event.type == pygame.KEYDOWN:
-                if event.key == keys[letter_index]:
-                    time_keys.append([letters[letter_index], datetime.now()])
-                    letter_index += 1
-                    letter_index %= len(letters)
-                    
-        screen.fill((205, 205, 205))
-        text = render_ront(letters[letter_index])
-        screen.blit(text, (50, 50))
-        pygame.display.flip();                    
+# Read the preset file, and send it to the board. This file was stolen from the BCI GUI saved file
+preset_file = "preset.txt"
+preset_file_contents = ""
+with open(preset_file) as fp:
+    preset_file_contents = fp.read()
+
+print(preset_file_contents)
+board.config_board(preset_file_contents)
+
+#start the data stream
+board.start_stream(45000)
+
+time.sleep(5)  # recommended window size for eeg metric calculation is at least 4 seconds, bigger is better
+data = np.array(board.get_board_data())
 
 
-start_window()
+analog_channel = board.get_analog_channels(board_id)
+eeg_channels = board.get_eeg_channels(board_id) 
+eeg_names = board. get_eeg_names(board_id)
+accel_channel = board.get_accel_channels(board_id)
 
-with open("test.csv", "w") as fp:
-    for input in time_keys:
-        fp.write(f"{input[0]},{input[1]}\n")
+
+print(f"eeg: {eeg_channels}")
+print(f"eeg names: {eeg_names}")
+print(f"analog channels: {analog_channel}")
+print(f"acceleration channels: {accel_channel}")
+print(f"data shape: {data.shape}")
+board.stop_stream()
+
+for i in eeg_channels:
+    print(i, end=": ")
+    print(data[i][250], end = ", ")
+    print(data[i][500], end = ", ")
+    print(data[i][750])
+
+quit()

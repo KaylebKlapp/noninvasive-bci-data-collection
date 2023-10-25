531dbe9a3a02c728314c5a660cad4c9f9029821c
sambrennan16 <c24samuel.brennan@afacademy.af.edu>
2023-10-25 10:17:31 -0600

    Prints. Working on Asthetics

--- odd_character_sim.py
+++ odd_character_sim.py
@@ -6,17 +6,19 @@ import brainflow as bf
 import argparse
 import random
 
-words_file = open("words.txt")
-words_file_contents = words_file.read()
-words = words_file_contents.split("\n")
-print(words)
-
-
-
+# Initialize Pygame Things
 pygame.init()
 pygame.font.init()
-font = pygame.font.SysFont("Alata", 700)
-fonts = pygame.font.get_fonts()
+pygame.display.set_caption("BCI training")
+
+KEY_WORD_PROB = 0.3             #How often a word with a key letter is used
+NORMAL_CHAR_COLOR = (0, 0, 0)   #Color of all non-odd letters, Defaut it black. Do not change.
+
+FONT = pygame.font.SysFont("Alata", 50)
+FONTS = pygame.font.get_fonts()
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 550
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 time_keys = []
 alphabet = [chr(i) for i in range(ord("A"), ord("Z"))]
@@ -28,18 +30,49 @@ for let in training_letters:
     
 keys = [pygame.K_k, pygame.K_w, pygame.K_e]
 
-def get_random_color():
-    return (random.randrange(256), random.randrange(256), random.randrange(256))
+words_file_with_keys = open("words_with_keys.txt", "r")
+words_file_with_keys_contents = words_file_with_keys.read()
+words_with_keys = words_file_with_keys_contents.split("\n")
+words_file_with_keys.close()
 
-def render_ront(str, font, color = (0, 0, 0)):
-    text = font.render(str, True, (0,0,0))
+words_file_without_keys = open("words_without_keys.txt", "r")
+words_file_without_keys_contents = words_file_without_keys.read()
+words_without_keys = words_file_without_keys_contents.split("\n")
+words_file_without_keys.close()
+
+def get_random_word(words):
+    return random.choice(words)
+
+def get_random_wordbank():
+    if random.random() < KEY_WORD_PROB:
+        return words_without_keys
+    else:
+        return words_with_keys
+
+def get_random_color(minimum_color = 50, maximum_color = 215):
+    return (random.randint(minimum_color, maximum_color), random.randint(minimum_color, maximum_color), random.randint(minimum_color, maximum_color))
+
+def render_font(str, font, color):
+    text = font.render(str, True, color)
     return text
 
+def render_word(word, odd_char_color, odd_char_index):
+    for i in range(len(word)):
+        if i == odd_char_index:
+            text = render_font(word[i], FONT, odd_char_color)
+            SCREEN.blit(text, (i*15+25, SCREEN_HEIGHT/2))
+        else:
+            text = render_font(word[i], FONT, NORMAL_CHAR_COLOR)
+            SCREEN.blit(text, (i*15+25, SCREEN_HEIGHT/2))
+
 def start_window():
-    pygame.display.set_caption("BCI training")
-    screen = pygame.display.set_mode((550, 500))
+
+    word_bank = get_random_wordbank()
+    word = get_random_word(word_bank)
+    odd_char_color = (255, 0, 0)    #Color of odd letter. Default is red
+    odd_char_index = 0              #Index of odd char
     
-    letter_index = 0
+    #letter_index = 0
     running = True;
     while running:
         for event in pygame.event.get():
@@ -47,16 +80,25 @@ def start_window():
                 running = False;
                 break
             elif event.type == pygame.KEYDOWN:
+                odd_char_color = get_random_color()
+                word = get_random_word(word_bank)
+                odd_char_index = random.randint(0,len(word)-1)
+                print(odd_char_index)
+
+                """
+                if event.key == pygame.K_RETURN:
+                    time_keys.append([training_letters[letter_index], int(time.time() * 1000)])
+                    letter_index += 1
+                    letter_index %= len(training_letters)
+                
                 if event.key == keys[letter_index]:
                     time_keys.append([training_letters[letter_index], int(time.time() * 1000)])
                     letter_index += 1
                     letter_index %= len(training_letters)
-                    
-        screen.fill((205, 205, 205))
-        text = render_ront(training_letters[letter_index], font)
-        screen.blit(text, (50, 50))
-        pygame.display.flip();   
-    words_file.close()                 
+                """
+        SCREEN.fill((205, 205, 205))
+        render_word(word, odd_char_color, odd_char_index)
+        pygame.display.flip();                    
 
 start_window()
 """
